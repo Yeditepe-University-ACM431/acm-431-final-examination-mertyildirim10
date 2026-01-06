c32ea075a23affa06a6e99975b0b542e5c3fa3a2
mertyildirim10 <mertyildirim17403@gmail.com>
2026-01-06 15:19:56 +0300

    Update TaskRepository.kt

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -7,7 +7,16 @@ class TaskRepository(private val api: TaskApi) {
 
     suspend fun fetchTasks(): List<Task> {
         // TODO 1: Call API
+        val dtoList = api.getTasks()
+
         // TODO 2: Convert TaskDto list to Task list
-        return TODO("Provide the return value")
+        return dtoList.map { dto ->
+            Task(
+                id = dto.id,
+                title = dto.title,
+                isCompleted = dto.completed
+            )
+        }
     }
 }
+
